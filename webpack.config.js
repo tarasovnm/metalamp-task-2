d07edbcbb67e935f13cc83b589f8044525871999
tarasovnm <tarasovnm@gmail.com>
2021-02-22 16:23:53 +0400

    Added markup and style for Bullet list, Advantages list and Review item. Reworked like button.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 const fs = require('fs');
 
@@ -24,7 +25,12 @@ module.exports = {
     ...PAGES.map(page => new HtmlWebpackPlugin({
       template: `${PAGES_DIR}/${page}`,
       filename: `./${page.replace(/\.pug/, '.html')}`
-    }))
+    })),
+    new CopyPlugin({
+      patterns: [
+        { from: "pug/blocks/**/*.jpg", to: "img/[name].[ext]" },
+      ],
+    }),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -44,11 +50,27 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: ['file-loader'],
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: 'img',
+            outputPath: 'img',
+            useRelativePath: true
+          }
+        },
       },
       {
         test: /\.(ttf|woff|woff2|eot|svg)$/i,
-        use: ['file-loader'],
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: 'fonts',
+            outputPath: 'fonts',
+            useRelativePath: true
+          }
+        },
       }
     ],
   },
