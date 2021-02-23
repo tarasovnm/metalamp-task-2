9570ac70a5874a0f3fb042cce190f71157ad9492
tarasovnm <tarasovnm@gmail.com>
2021-02-23 13:57:23 +0400

    Added base functionality to dropdowns.

--- src/pug/blocks/dropdown/dropdown.js
+++ src/pug/blocks/dropdown/dropdown.js
@@ -1,13 +1,34 @@
 export default function initDropdowns() {
-  console.log('Dropdowns initialization');
+  const dropdowns = document.querySelectorAll('.dropdown');
 
-  let dropdowns = document.querySelectorAll('.dropdown')
   dropdowns.forEach(dropdown => {
     const head = dropdown.querySelector('.dropdown__head');
+    const options = dropdown.querySelectorAll('.option');
+
     head.addEventListener('click', e => {
-      console.log('Dropdown head is clicked');
       let parent = e.target.closest('.dropdown');
       parent.classList.toggle('dropdown--opened');
+    });
+
+    options.forEach(option => {
+      const plusButton = option.querySelector('.option__button--plus');
+      const minusButton = option.querySelector('.option__button--minus');
+
+      plusButton.addEventListener('click', e => {
+        let countElement = e.target.parentNode.querySelector('.option__count');
+        let countValue = Number(countElement.innerHTML);
+        if (countValue < 9) {
+          countElement.innerHTML = countValue + 1;
+        }
+      });
+
+      minusButton.addEventListener('click', e => {
+        let countElement = e.target.parentNode.querySelector('.option__count');
+        let countValue = Number(countElement.innerHTML);
+        if (countValue > 0) {
+          countElement.innerHTML = countValue - 1;
+        }
+      });
     });
   });
 }
